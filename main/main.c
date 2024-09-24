2ff5cc18360b47740a19a1fd4f3c30132b8eb56a
Li Junchen <lijunchen95@outlook.com>
2024-09-24 21:51:54 +0800

    x

--- main/main.c
+++ main/main.c
@@ -16,9 +16,6 @@
 #include "st7789.h"
 #include <string.h>
 
-#include "wasm3.h"
-#include "fib32.wasm.h"
-
 #include "runtime.h"
 #include "wasm0.h"
 #include "window.h"
@@ -102,42 +99,6 @@ void ST7789(void *pvParameters)
 
 #define FATAL(msg, ...) { printf("Fatal: " msg "\n", ##__VA_ARGS__); return; }
 
-static void run_wasm(void)
-{
-    M3Result result = m3Err_none;
-
-    uint8_t* wasm = (uint8_t*)fib32_wasm;
-    uint32_t fsize = fib32_wasm_len;
-
-    printf("Loading WebAssembly...\n");
-    IM3Environment env = m3_NewEnvironment ();
-    if (!env) FATAL("m3_NewEnvironment failed");
-
-    IM3Runtime runtime = m3_NewRuntime (env, 1024, NULL);
-    if (!runtime) FATAL("m3_NewRuntime failed");
-
-    IM3Module module;
-    result = m3_ParseModule (env, &module, wasm, fsize);
-    if (result) FATAL("m3_ParseModule: %s", result);
-
-    result = m3_LoadModule (runtime, module);
-    if (result) FATAL("m3_LoadModule: %s", result);
-
-    IM3Function f;
-    result = m3_FindFunction (&f, runtime, "fib");
-    if (result) FATAL("m3_FindFunction: %s", result);
-
-    printf("Running...\n");
-
-    result = m3_CallV(f, 24);
-    if (result) FATAL("m3_Call: %s", result);
-
-    unsigned value = 0;
-    result = m3_GetResultsV (f, &value);
-    if (result) FATAL("m3_GetResults: %s", result);
-
-    printf("Result: %u\n", value);
-}
 
 extern unsigned char __tinypong_wasm[];
 extern unsigned int __tinypong_wasm_len;
@@ -223,9 +184,6 @@ void app_main(void)
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
     printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    // printf("\nWasm3 v" M3_VERSION " on " CONFIG_IDF_TARGET ", build " __DATE__ " " __TIME__ "\n");
-    // run_wasm();
 
     // create gpio button
     {
