5dc11127a9674e4f129ce67059664ddea17cda5c
Li Junchen <lijunchen95@outlook.com>
2024-11-06 10:49:16 +0800

    seem not good

--- main/display.c
+++ main/display.c
@@ -63,31 +63,39 @@ uint16_t convert(uint32_t c) {
   return color;
 }
 
+uint8_t g(const uint8_t* framebuffer, int x, int y) {
+  int n = y * 160 + x;
+  int index = n / 4;
+  int m = index % 4;
+  uint8_t quartet = framebuffer[index];
+  if (m == 0) {
+    return quartet & 0b00000011;
+  } else if (m == 1) {
+    return (quartet & 0b00001100) >> 2;
+  } else if (m == 2) {
+    return (quartet & 0b00110000) >> 4;
+  } else {
+    return (quartet & 0b11000000) >> 6;
+  }
+}
+
 void w4_windowComposite(const uint32_t* palette, const uint8_t* framebuffer) {
   // Convert indexed 2bpp framebuffer to XRGB output
   // uint32_t* out = pixels;
-  for (int n = 0; n < 160 * 160 / 4; ++n) {
-    uint8_t quartet = framebuffer[n];
-    int color1 = (quartet & 0b00000011) >> 0;
-    int color2 = (quartet & 0b00001100) >> 2;
-    int color3 = (quartet & 0b00110000) >> 4;
-    int color4 = (quartet & 0b11000000) >> 6;
-
-    uint16_t c1 = convert(palette[color1]);
-    uint16_t c2 = convert(palette[color2]);
-    uint16_t c3 = convert(palette[color3]);
-    uint16_t c4 = convert(palette[color4]);
-    uint16_t cs[4] = {c1, c2, c3, c4};
-
-    for (int i = n * 4; i < n * 4 + 4; i++) {
-      int x = i % 160;
-      int y = i / 160;
-      uint16_t c = cs[i - n * 4];
-      if (c != last[i]) {
-        lcdDrawPixel(&dev, x + 40, y + 40, c);
-        last[i] = c;
+  for (int y = 0; y < 160; y++) {
+    uint8_t last_color = g(framebuffer, 0, y);
+    uint8_t last_x = 0;
+    for (int x = 1; x < 160; x++) {
+      uint8_t cur_color = g(framebuffer, x, y);
+      if (cur_color != last_color) {
+        lcdDrawFillRect(&dev, last_x + 40, y + 40, x + 40, y + 40,
+                        convert(palette[last_color]));
+        last_x = x;
+        last_color = cur_color;
       }
     }
+    lcdDrawFillRect(&dev, last_x + 40, y + 40, 160 + 40, y + 40,
+                    convert(palette[last_color]));
   }
 }
 
